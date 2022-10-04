7efc6dc79b2006e3e3663b6067a2e6ad5a7ff822
JulieBodette <jr3python@gmail.com>
2022-10-04 14:32:03 -0400

    Added basic html and CSS for the login/signup button

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ export function Header() {
     <header className="header">
       <img className="header__image" src={header} alt="Around the US" />
       {/*we acess the image via variable name {header}*/}
+      <p className="header__login-text">Login or sign up</p>
     </header>
   );
 }
