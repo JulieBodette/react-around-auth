8ffabd00396d7322dc1f16bf90f811e047158e81
JulieBodette <jr3python@gmail.com>
2022-10-03 21:34:56 -0400

    added auth.js. It is NOT connected to the rest of the code and has not been tested.

---
+++ src/utils/auth.js
@@ -0,0 +1,42 @@
+//TO DO: TEST THIS CODE!!!
+
+const BASEURL = "https://register.nomoreparties.co";
+const HEADERS = {"Content-Type": "application/json"}
+
+processResponse = (res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  };
+
+//TO DO: manually set info to the username and password for testing
+//ie {"password": "somepassword", "email": "email8@email.com"}
+function SignUp(info) {
+    const url = BASEURL + "/signup";
+    return fetch(url, {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify(info),
+    })
+      .then(processResponse())
+      .catch((err) => {
+        console.log(err); // log the error to the console
+      });
+  }
+
+  function SignIn(info) {
+    const url = BASEURL + "/signin";
+    return fetch(url, {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify(info),
+    })
+      .then(processResponse())
+      .catch((err) => {
+        console.log(err); // log the error to the console
+      });
+  }
+
+
+
