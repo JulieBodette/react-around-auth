3935ff08bebfe1506c82bd4106ca1f322f7ba6f1
JulieBodette <jr3python@gmail.com>
2022-10-05 16:29:19 -0400

    Fixed Log in text syle, renamed loginform to be loginform, changed username to email

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,13 +5,13 @@ export function Login({ handleSubmit }) {
 
 return(
 
-<form name="avatarimage" className="login__form login" onSubmit={handleSubmit}>
-  <h1>Log in</h1>
+<form name="loginform" className="login__form login" onSubmit={handleSubmit}>
+  <h1 className="login__title">Log in</h1>
         <input
           className="login__input-text"
-          name="username"
-          placeholder="Username"
-          id="username"
+          name="email"
+          placeholder="Email"
+          id="email"
           required
         />
         {/*Could add an error field here- similar to the modal errors- maybe check to make sure the test is an email (has @ sign?) */}
