7bfae31ff8fe3dfc4c125c4e250d56e659dcb33d
JulieBodette <jr3python@gmail.com>
2022-10-12 16:22:51 -0400

    JWT Token is saved to localStorage on login

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -4,6 +4,7 @@ const HEADERS = {"Content-Type": "application/json"}
 
 const processResponse = (res) => {
     if (res.ok) {
+     
       return res.json();
     }
     return Promise.reject(`Error: ${res.status}`); //same as throwing error- except the object that goes into the catch block is a string, not an error object
@@ -30,7 +31,12 @@ export function signUp(info) {
       headers: HEADERS,
       body: JSON.stringify(info),
     })
-    .then(processResponse)
+    .then(res => res.json())
+    .then((data) => {
+     // saving the token
+     localStorage.setItem('token', data.token);
+     console.log(data.token);
+     })
       .catch((err) => {
         console.log(err); // log the error to the console
         return Promise.reject(err); //pass the error along to the next .catch as if we hadn't caught it
